387ae04116cd22b708abe78d9d268bf2924d99eb
seregareptiloid <76225061+seregareptiloid@users.noreply.github.com>
2021-04-04 20:21:55 +0600

    Refactoring

--- cvtest/Program.cs
+++ cvtest/Program.cs
@@ -13,6 +13,7 @@ namespace cvtest
             Emgu.CV.CvEnum.TemplateMatchingType matchingType = Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed;
             Image<Gray, Single> templateImage = new Image<Gray, Single>(@"C:\Users\shabir\source\repos\cvtest\cvtest\data\Template.png");
             Image<Gray, Single> changedImage = new Image<Gray, Single>(@"C:\Users\shabir\source\repos\cvtest\cvtest\data\Changed.png");
+            Image<Gray, Single> defaultImage = new Image<Gray, Single>(@"C:\Users\shabir\source\repos\cvtest\cvtest\data\Default.png");
             Image<Gray, Single> imageToShow = changedImage.Copy();
             using (Image<Gray, Single> result = changedImage.MatchTemplate(templateImage, matchingType))
             {
@@ -21,14 +22,26 @@ namespace cvtest
                 result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
                 if (maxValues[0] > 0.999)
                 {
-                    // This is a match. Do something with it, for example draw a rectangle around it.
-                    Rectangle match = new Rectangle(maxLocations[0], templateImage.Size);
-                    imageToShow.Draw(match, new Gray(), 3);
-                    Console.WriteLine("True");
+                    Console.WriteLine("Changed image True");
                 }
                 else
                 {
-                    Console.WriteLine("False");
+                    Console.WriteLine("Changed image False");
+                }
+            }
+
+            using (Image<Gray, Single> result = defaultImage.MatchTemplate(templateImage, matchingType))
+            {
+                double[] minValues, maxValues;
+                Point[] minLocations, maxLocations;
+                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+                if (maxValues[0] > 0.999)
+                {
+                    Console.WriteLine("Unchanged image True");
+                }
+                else
+                {
+                    Console.WriteLine("Unchanged image False");
                 }
             }
         }
